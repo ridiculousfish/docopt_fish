6ad707e16158a85599f21eb9382669406e6f65d4
ridiculousfish <corydoras@ridiculousfish.com>
2015-05-05 19:09:54 -0700

    Simple benchmark to track down performance issues

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 TEST_SRC_FILES=docopt_fish.cpp docopt_fish_test.cpp docopt_fish_parse_tree.cpp
+BENCHMARK_SRC_FILES=docopt_fish.cpp docopt_fish_benchmark.cpp docopt_fish_parse_tree.cpp
 HEADERS=docopt_fish.h docopt_fish_grammar.h docopt_fish_types.h
 CXX=clang++
 CXXFLAGS=-O0 -g -W -Wall
@@ -6,8 +7,14 @@ CXXFLAGS=-O0 -g -W -Wall
 test: docopt_test
 	./docopt_test
 
+benchmark: docopt_benchmark
+	./docopt_benchmark
+
 docopt_test: ${TEST_SRC_FILES:.cpp=.o} ${HEADERS}
 	${CXX} ${TEST_SRC_FILES:.cpp=.o} -o $@
+
+docopt_benchmark: ${BENCHMARK_SRC_FILES:.cpp=.o} ${HEADERS}
+	${CXX} ${BENCHMARK_SRC_FILES:.cpp=.o} -o $@
 
 python_test: run_testcase
 	python ./run_tests.py
@@ -16,7 +23,7 @@ run_testcase: ${PY_TEST_SRC_FILES:.cpp=.o} ${HEADERS}
 	${CXX} ${PY_TEST_SRC_FILES:.cpp=.o} -o $@
 
 clean:
-	rm -f run_testcase docopt_test *.o
+	rm -f run_testcase docopt_test docopt_benchmark *.o
 
 %.o: %.cpp
 	${CXX} ${CXXFLAGS} $^ -c
---
+++ docopt_fish_benchmark.cpp
@@ -0,0 +1,68 @@
+#include <string>
+#include <vector>
+#include <cassert>
+#include <sys/time.h>
+#include "docopt_fish.h"
+
+using namespace std;
+using namespace docopt_fish;
+
+static const char * const g_bind_usage =
+"Usage:\n"
+"    bind [-M <MODE> | --mode <MODE>] [-m <NEW_MODE> | --sets-mode <NEW_MODE>]\n"
+"         [-k | --key] <SEQUENCE> <COMMAND>...\n"
+"    bind [-M <MODE> | --mode <MODE>] [-k | --key] <SEQUENCE>\n"
+"    bind (-f | --function-names)\n"
+"    bind (-K | --key-names) [(-a | --all)]\n"
+"    bind (-e | --erase) [-M <MODE> | --mode <MODE>]\n"
+"         [-a | --all] [-k | --key] [<SEQUENCE>...]\n"
+"    bind (-h | --help)\n"
+"Options:\n"
+"    -k, --key                                Specify a key name, such as 'left' or 'backspace' instead of a character sequence.\n"
+"    -K, --key-names                          Display a list of available key names. Specifying -a or --all"
+"                                             includes keys that don't have a known mapping\n"
+"    -M <MODE>, --mode <MODE>                 Specify a bind mode that the bind is used in. Defaults to 'default'\n"
+"    -m <NEW_MODE>, --sets-mode <NEW_MODE>    Change the current mode to NEW_MODE after this binding is executed\n"
+"    -e, --erase                              Erase the binding with the given sequence and mode instead of defining a new one.\n"
+"                                             Multiple sequences can be specified with this flag. Specifying -a or --all with -M or --mode\n"
+"                                             erases all binds in the given mode regardless of sequence. Specifying -a or --all\n"
+"                                             without -M or --mode erases all binds in all modes regardless of sequence.\n"
+"    -a, --all                                See --erase and --key-names.\n"
+;
+
+
+static double timef()
+{
+    int time_res;
+    struct timeval tv;
+    
+    time_res = gettimeofday(&tv, 0);
+    
+    if (time_res)
+    {
+        return -1;
+    }
+    
+    return (double)tv.tv_sec + 0.000001*tv.tv_usec;
+}
+
+
+int main(void)
+{
+    argument_parser_t<string> parser;
+    bool parsed = parser.set_doc(g_bind_usage, NULL);
+    assert(parsed);
+
+    vector<string> argv;
+    argv.push_back("abc");
+    argv.push_back("forward-word");
+    
+    double before = timef();
+    size_t amt = 1000;
+    for (size_t i=0; i < amt; i++) {
+        parser.parse_arguments(argv, flags_default, NULL, NULL);
+    }
+    double after = timef();
+    fprintf(stderr, "msec per: %f\n", (after - before) * (1000.0) / amt);
+    return 0;
+}
